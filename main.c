03d72569243116285d27b3a8f91bded9061ecf94
vik <vs7798@outlook.com>
2018-04-07 22:19:27 -0700

    Done upto creation of database

--- main.c
+++ main.c
@@ -1,6 +1,79 @@
+
+#include<stdlib.h>
 #include<stdio.h>
+
+// function to create seats
 
 int main(){
 
-printf(" Page Something" );
+printf("Welcome to Airline Database Management System \n");
+printf("1) Create Databases \n");
+printf("2) Open Databases  \n");
+printf("3) Remove Databases \n");
+
+char option;
+option = getchar();
+
+
+// create database
+if(option == '1'){
+printf("Create database \n");
+
+
+int row =0;
+printf("Enter number of rows in your plane \n");
+scanf(" %d" , &row);
+int col = 5;
+
+int  Rseats[row];
+char  Cseats[5] = {'A', 'B' , 'C', 'D'};
+
+
+// Creates table here
+for(int i=0; i<row; i++){
+int go = i+1;
+Rseats[i] = go;
 }
+
+//View table
+for(int k=0; k<row; k++){
+printf(" %i " , Rseats[k]);
+for(int l=0; l<col; l++){
+printf(" %c " , Cseats[l]);
+}
+printf(" \n");
+}
+
+printf("Database Created Sucessfully\n");
+printf("Enter unique name for this datbase \n");
+char databaseName[100];
+scanf(" %s" , databaseName);
+puts(databaseName);
+}
+
+
+
+// open database
+else if(option =='2'){
+printf("Select Database you want to open \n");
+}
+
+
+// delete database
+else if (option == '3'){
+  printf("Select Database you want to remove \n");
+}
+
+else{
+  printf("Invalid Input \n");
+}
+
+
+
+return 0;
+}
+
+
+
+
+
---
+++ test.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+int main(){
+
+int aa = 4;
+
+char bb[155];
+
+ scanf("%s" , bb);
+}
+
